fa8ec24f1ad7d9897883401f84dd7d3343859e3c
sushantbehere <sushantbehere2001@gmail.com>
2023-03-20 13:40:08 +0530

    Final Commit

---
+++ app/src/main/java/com/txon/quizapp/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package com.txon.quizapp;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Name the planet known as the Red Planet",
+            "Who invented electricity",
+            "How many millimetres are there in a centimetre",
+            "Name the hardest substance available on Earth"
+
+    };
+
+    public static String choices[][] = {
+            {"Jupiter", "Venus" , "Mars", "Moon"},
+            {"Benjamin Franklin", "Thomas Edison" , "Isaac Newton" , "Peter Henlein"},
+            {"20mm","15mm","10mm","25mm"},
+            {"Gold","Silicon","Copper","Diamond"}
+
+    };
+
+    public static String correctAnswers[] = {
+            "Mars",
+            "Benjamin Franklin",
+            "10mm",
+            "Diamond"
+
+    };
+
+}
